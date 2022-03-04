3d40cb787250f6ca2bbfc98c9a52a6d4e6b4f449
Owen Yoshishige <owenyoshishige@gmail.com>
2022-03-03 17:21:10 -0700

    Complete N-Queens BMR

--- src/solvers.js
+++ src/solvers.js
@@ -16,23 +16,67 @@
 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  var solution = new Board({n: n}); //fixme
+  console.log(solution);
+
+  for (let index = 0; index < solution.attributes.n; index++) { //places n rooks on the first major diagonal
+    solution.togglePiece(index, index); // rooks on a major diagonal will always work
+  }
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  return solution.rows();
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  return n > 1 ? n * window.countNRooksSolutions(n - 1) : 1;
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
-  return solutionCount;
 };
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
-  var solution = undefined; //fixme
+  var solution; //fixme
+  var solutionBoard = new Board({n: n});
+  if (n === 2 || n === 3) {
+    return solutionBoard.rows();
+  }
+
+  //Interpretation: need to return a solution board
+
+  //Input - n
+  //Output - board with valid queen positions
+  //Constraints - none
+  //Edgecases - 0, 2 and 3
+
+  //strategy
+  //within our for loop have an if (solution) === if solution is defined
+  //note: kind of copied countNQueens
+
+  //Psuedocode
+  //if 0, 2, or 3.
+  //return empty array
+
+  //make solution
+
+  let queenHelper = function (row = 0) {
+    if (row === n) { //row is equivalent to number of queens
+      solution = solutionBoard.rows();
+      return;
+    }
+    for (let col = 0; col < n; col++) {
+      solutionBoard.togglePiece(row, col); //we place a queen
+      if (!solutionBoard.hasAnyQueensConflicts()) { //the requirement for a recursive call is no conflicts on the board
+        queenHelper(row + 1);
+      }
+      if (solution) {
+        return;
+      }
+      solutionBoard.togglePiece(row, col);
+    }
+  };
+  queenHelper();
+
 
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
   return solution;
@@ -40,7 +84,42 @@ window.findNQueensSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  if (n === 2 || n === 3) {
+    return 0;
+  }
+  var solutionCount = 0;
+  var solutionBoard = new Board({n: n});
+
+
+  //input: n
+  //output: an integer representing the total possible variations for having n queens on an n x n board
+  //constraints: time ?
+  //edge cases: 2 and 3
+
+  //Helperfunc
+  //base case:
+  //to keep recursing:
+
+  //Psuedocode
+  //create helperfunction with param of row
+  //and a for loop that iterates through the column index
+  //for each recursive call of the function, we test every possible column for conflicts
+  //When we find a column where we can place a queen with no conflicts, we recursively call the function with row + 1
+  //essentially taking us to the next row, where we iterate through the columns and try to find another column index on the new row with no conflicts
+  let queenHelper = function (row = 0) {
+    if (row === n) { //row is equivalent to number of queens
+      solutionCount++;
+      return;
+    }
+    for (let col = 0; col < n; col++) {
+      solutionBoard.togglePiece(row, col); //we place a queen
+      if (!solutionBoard.hasAnyQueensConflicts()) { //the requirement for a recursive call is no conflicts on the board
+        queenHelper(row + 1);
+      }
+      solutionBoard.togglePiece(row, col);
+    }
+  };
+  queenHelper();
 
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
